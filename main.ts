b9c07b3bec8305b7f88eb4667da6efef40357d53
DanielSLP <64851517+DanielSLP@users.noreply.github.com>
2022-05-23 22:48:54 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,32 @@
-function pošliposly() {
-    
-    if (ESP8266_IoT.wifiState(false)) {
+function pošliposly () {
+    while (ESP8266_IoT.wifiState(false)) {
         basic.showNumber(3)
         ESP8266_IoT.connectWifi("Host", "wifihost")
         basic.showNumber(4)
     }
-    
-    if (ESP8266_IoT.thingSpeakState(false)) {
+    while (ESP8266_IoT.thingSpeakState(false)) {
         basic.showNumber(5)
         ESP8266_IoT.connectThingSpeak()
         basic.showNumber(6)
     }
-    
     basic.showNumber(7)
-    ESP8266_IoT.setData("3EDHAXEIWOUZXRZ1", avg_prasnost, Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C), Environment.octopus_BME280(Environment.BME280_state.BME280_humidity), Environment.octopus_BME280(Environment.BME280_state.BME280_pressure), Environment.ReadLightIntensity(AnalogPin.P2))
+    ESP8266_IoT.setData(
+    "3EDHAXEIWOUZXRZ1",
+    avg_prasnost,
+    Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_pressure),
+    Environment.ReadLightIntensity(AnalogPin.P2)
+    )
     basic.showNumber(8)
+    ESP8266_IoT.connectWifi("Host", "wifihost")
+    ESP8266_IoT.connectThingSpeak()
     ESP8266_IoT.uploadData()
     basic.showNumber(9)
     odeslano += 1
     basic.clearScreen()
 }
-
-function OLED_vypis() {
+function OLED_vypis () {
     OLED.clear()
     OLED.writeString("Prasnost<0.8μm:")
     OLED.writeNumNewLine(prasnost)
@@ -44,7 +49,6 @@ function OLED_vypis() {
     OLED.writeString(" odsl:")
     OLED.writeNum(odeslano)
 }
-
 let i = 0
 let odeslano = 0
 let avg_prasnost = 0
@@ -58,8 +62,7 @@ pocet_minut = 0.0166667
 OLED.init(128, 64)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 basic.showNumber(2)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     prasnost = Environment.ReadDust(DigitalPin.P16, AnalogPin.P1)
     i += 1
     avg_prasnost = (avg_prasnost * (i - 1) + prasnost) / i
@@ -71,9 +74,8 @@ basic.forever(function on_forever() {
     if (pocet_minut < 0) {
         pošliposly()
         max_prasnost20m = 0
-        pocet_minut = 20.0166667
+        pocet_minut = 1
         i = 0
         avg_prasnost = 0
     }
-    
 })
